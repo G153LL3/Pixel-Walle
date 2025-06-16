aa53d69cd4009d64ef134cf301b31bcb8ca7e9ec
G153LL3 <yisellvelazquezochoa@gmail.com>
2025-06-16 11:28:32 -0400

    Add save and load

--- Scripts/MainScript.cs
+++ Scripts/MainScript.cs
@@ -2,24 +2,111 @@ using Godot;
 
 public partial class MainScript : Node
 {
-    public CodeEdit miEditor;
-    public Button miBoton;
+    public CodeEdit codeEdit; 
+
+    public Button runButton;
+    public Button saveButton;
+    public Button loadButton;
+
+    SpinBox spinBox;
+
+    public FileDialog fileDialogSave;
+	public FileDialog fileDialogLoad;
 
     public override void _Ready()
     { 
-        miEditor = GetNode<CodeEdit>("CodeEdit");
-        miBoton = GetNode<Button>("RunButton");
-        miBoton.Pressed += EjecutarCodigo;
+        codeEdit = GetNode<CodeEdit>("CodeEdit");
+
+        runButton = GetNode<Button>("RunButton");
+		saveButton = GetNode<Button>("SaveButton");
+		loadButton = GetNode<Button>("LoadButton");
+
+        spinBox = GetNode<SpinBox>("SpinBox");
+
+        runButton.Pressed += OnRunPressed;
+		saveButton.Pressed += OnSavePressed;
+		loadButton.Pressed += OnLoadPressed;
+
+       // spinBox.ValueChanged += OnSpinBoxValueChanged;
+
+
+        fileDialogSave = new FileDialog();
+        fileDialogSave.Access = FileDialog.AccessEnum.Filesystem;
+        fileDialogSave.FileMode = FileDialog.FileModeEnum.SaveFile;
+        fileDialogSave.Filters = new string[] { "*.gw" };
+		AddChild(fileDialogSave);
+		fileDialogSave.FileSelected += _OnFileDialogSaveFileSelected;
+
+        fileDialogLoad = new FileDialog();
+		fileDialogLoad.Access = FileDialog.AccessEnum.Filesystem;
+		fileDialogLoad.FileMode = FileDialog.FileModeEnum.OpenAny;
+		fileDialogLoad.Filters = new string[] { "*.gw" };
+        AddChild(fileDialogLoad);
+		fileDialogLoad.FileSelected += _OnFileDialogLoadFileSelected;
+	
+
         Program.Initialize();
     }
 
-    private void EjecutarCodigo()
+    private void OnRunPressed()
     {
-        string codigo = miEditor.Text;
+        string codigo = codeEdit.Text;
         string resultado = Program.Execute(codigo);
-
         GD.Print("Resultado de la ejecución:\n" + resultado);
+    }
+    public void OnSavePressed()
+	{
+        fileDialogSave.PopupCentered();
+        fileDialogSave.Size = new Vector2I(600, 400); 
+	}
+    public void OnLoadPressed()
+	{
+        fileDialogLoad.PopupCentered();
+        fileDialogLoad.Size = new Vector2I(600, 400);
 
+	}
+    /*
+    public void OnSpinBoxValueChanged(double newValue)
+	{
+		Reset();
+		Dimension = (int)newValue;
+	}
+    */
+    private void _OnFileDialogSaveFileSelected(string ruta)
+    {
+        GuardarArchivo(ruta);
+        GD.Print("Archivo guardado en: " + ruta);
+    }
+    private void _OnFileDialogLoadFileSelected(string ruta)
+    {
+        CargarArchivo(ruta);
+        GD.Print("Archivo cargado desde: " + ruta);
+    }
+    private void GuardarArchivo(string ruta)
+	{
+		using var archivo = FileAccess.Open(ruta, FileAccess.ModeFlags.Write);
+        if (archivo != null)
+        {
+            archivo.StoreString(codeEdit.Text);
+            GD.Print("Guardado exitoso");
+        }
+        else
+        {
+            GD.PrintErr("Error al guardar: " + FileAccess.GetOpenError());
+        }
+	}
+    private void CargarArchivo(string ruta)
+    {
+         using var archivo = FileAccess.Open(ruta, FileAccess.ModeFlags.Read);
+        if (archivo != null)
+        {
+            codeEdit.Text = archivo.GetAsText();
+            GD.Print("Carga exitosa");
+        }
+        else
+        {
+            GD.PrintErr("Error al cargar: " + FileAccess.GetOpenError());
+        }
     }
     
 }
